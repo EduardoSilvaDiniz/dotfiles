2f736005c376ca1c364eedd7c6a1b1e44b9c1d25
Eduardo Silva <eduardofox989@protonmail.com>
2025-11-29 14:31:54 -0300

    updated init.el

--- emacs-from-scratch/init.el
+++ emacs-from-scratch/init.el
@@ -1,17 +1,57 @@
 ;; comando eval-buffer executa tudo do arquivo
 ;; C-x + C-e executa linha
-(setq inhibit-startup-message t)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(tooltip-mode -1)
-(set-fringe-mode 10)
+(use-package emacs
+  :custom
+  (menu-bar-mode nil)         ;; Disable the menu bar
+  (scroll-bar-mode nil)       ;; Disable the scroll bar
+  (tool-bar-mode nil)         ;; Disable the tool bar
+  (inhibit-startup-screen t)  ;; Disable welcome screen
 
-(menu-bar-mode -1)
+  (delete-selection-mode t)   ;; Select text and delete it by typing.
+  ;; (electric-indent-mode nil)  ;; Turn off the weird indenting that Emacs does by default.
+  (electric-pair-mode t)      ;; Turns on automatic parens pairing
 
-(setq visible-bell t)
+  ;; (blink-cursor-mode nil)     ;; Don't blink cursor
+  (global-auto-revert-mode t) ;; Automatically reload file and show changes if the file has changed
 
-(column-number-mode)
-(global-display-line-numbers-mode t)
+  ;;(global-visual-line-mode t)           ;; Enable truncated lines
+  ;;(display-line-numbers-type 'relative) ;; Relative line numbers
+  (global-display-line-numbers-mode t)  ;; Display line numbers
+	(global-hl-line-mode t) ;; destaca a linha que o cursor está
+
+
+  (mouse-wheel-progressive-speed nil) ;; Disable progressive speed when scrolling
+  (scroll-conservatively 10) ;; Smooth scrolling
+  ;;(scroll-margin 8)
+
+  (tab-width 2)
+
+  (make-backup-files nil) ;; Stop creating ~ backup files
+  (auto-save-default nil) ;; Stop creating # auto save files
+
+
+	;;minhas configs
+	(tooltip-mode nil)
+	(set-fringe-mode 10)
+	(custom-safe-themes t)
+	(visible-bell t)
+	(column-number-mode)
+
+  :hook
+  (prog-mode . (lambda () (hs-minor-mode t))) ;; Enable folding hide/show globally
+  :config
+  ;; Move customization variables to a separate file and load it, avoid filling up init.el with unnecessary variables
+  (setq custom-file (locate-user-emacs-file "custom-vars.el"))
+  (load custom-file 'noerror 'nomessage)
+  :bind (
+         ([escape] . keyboard-escape-quit) ;; Makes Escape quit prompts (Minibuffer Escape)
+         ;; Zooming In/Out
+         ("C-+" . text-scale-increase)
+         ("C--" . text-scale-decrease)
+         ("<C-wheel-up>" . text-scale-increase)
+         ("<C-wheel-down>" . text-scale-decrease)
+         ))
+
 ;; cria uma lista de modos chamada modo
 ;; define um hook (chamada automatica) que define
 ;; display-line-number-mode 0 (desativado)
@@ -22,9 +62,14 @@
 		eshell-mode-hook
 		shell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
-;;(set-face-attribute 'default nil :font "Fira Code Renita" :height 280)
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 140)
 
-;;(load-theme 'wombat)
+;; performance
+;; The default is 800 kilobytes. Measured in bytes.
+(setq gc-cons-threshold (* 50 1000 1000))
+
+;; For blazingly fast startup times, this line makes startup miles faster
+(setq package-quickstart t)
 
 ;; make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -44,21 +89,9 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; pacote para ter visualização em formato de lista para pesquisa comados, arquivos, pastas etc...
-;;(use-package ivy
-  ;;:diminish
-  ;;:bind (("C-s" . sniper))
-  ;;:config
-  ;;(ivy-mode 1))
-
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1))
-
-;;(use-package gruvbox-theme
-  ;;:config
-  ;;(setq gruvbox-bold-constructs t)
-  ;;(load-theme 'gruvbox-dark-medium t)) ;; We need to add t to trust this package
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
@@ -69,16 +102,11 @@
   :config
   (setq which-key-idle-delay 0.3))
 
-;;(use-package ivy-rich
-  ;;:init
-;;(ivy-rich-mode 1))
-
-;; alternativa ao ivy que usa funções do proprio emacs
 (use-package vertico
   :init
   (vertico-mode))
 
-(savehist-mode) ;; Enables save history mode
+(savehist-mode)
 
 (use-package marginalia
   :after vertico
@@ -94,3 +122,71 @@
 
 (use-package doom-themes
   :init(load-theme 'doom-gruvbox))
+
+(use-package evil
+  :init
+  (evil-mode)
+  :config
+  (evil-set-initial-state 'eat-mode 'insert) ;; Set initial state in eat terminal to insert mode
+  :custom
+  (evil-want-keybinding nil)    ;; Disable evil bindings in other modes (It's not consistent and not good)
+  (evil-want-C-u-scroll t)      ;; Set C-u to scroll up
+  (evil-want-C-i-jump nil)      ;; Disables C-i jump
+  (evil-undo-system 'undo-redo) ;; C-r to redo
+  ;; Unmap keys in 'evil-maps. If not done, org-return-follows-link will not work
+  :bind (:map evil-motion-state-map
+              ("SPC" . nil)
+              ("RET" . nil)
+              ("TAB" . nil)))
+(use-package evil-collection
+  :after evil
+  :config
+  ;; Setting where to use evil-collection
+  (setq evil-collection-mode-list '(dired ibuffer magit corfu vertico consult info))
+  (evil-collection-init))
+
+(use-package magit
+  :defer
+  :custom (magit-diff-refine-hunk (quote all)) ;; Shows inline diff
+  :config (define-key transient-map (kbd "<escape>") 'transient-quit-one))
+
+(use-package diff-hl
+  :hook ((dired-mode         . diff-hl-dired-mode-unless-remote)
+         (magit-post-refresh . diff-hl-magit-post-refresh))
+  :init (global-diff-hl-mode))
+
+(use-package org
+  :ensure nil
+  :custom
+  (org-edit-src-content-indentation 4) ;; Set src block automatic indent to 4 instead of 2.
+  (org-return-follows-link t)   ;; Sets RETURN key in org-mode to follow links
+  :hook
+  (org-mode . org-indent-mode))
+
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "/home/edu/org-roam/"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
+
+(use-package perfect-margin
+  :custom
+  (perfect-margin-visible-width 128)
+  :hook ((org-mode . perfect-margin-mode)
+         (after-change-major-mode . perfect-margin--maybe-disable)))
+
+(defun perfect-margin--maybe-disable ()
+  (unless (derived-mode-p 'org-mode)
+    (perfect-margin-mode -1)))
