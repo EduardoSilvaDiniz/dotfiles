57f1f2fcffd9c91665c1f7cf4f1b8204b6ef0c1c
Eduardo Silva <eduardofox989@protonmail.com>
2026-01-11 18:13:41 -0300

    configurações do notebook

--- emacs-from-scratch/init.el
+++ emacs-from-scratch/init.el
@@ -1,5 +1,16 @@
 ;; comando eval-buffer executa tudo do arquivo
 ;; C-x + C-e executa linha
+
+;; codigo	necessario para	o	haskell
+;; (setenv "PATH" (concat (getenv "PATH") ":" (expand-file-name "~/.ghcup/bin")))
+;; (add-to-list 'exec-path (expand-file-name "~/.ghcup/bin"))
+
+;; performance
+(setq package-quickstart t)
+(setq gc-cons-threshold (* 50 1000 1000))
+(setq gc-cons-threshold 100000000) ; 100MB
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+
 (use-package emacs
   :custom
   (menu-bar-mode nil)         ;; Disable the menu bar
@@ -8,21 +19,16 @@
   (inhibit-startup-screen t)  ;; Disable welcome screen
 
   (delete-selection-mode t)   ;; Select text and delete it by typing.
-  ;; (electric-indent-mode nil)  ;; Turn off the weird indenting that Emacs does by default.
   (electric-pair-mode t)      ;; Turns on automatic parens pairing
 
-  ;; (blink-cursor-mode nil)     ;; Don't blink cursor
   (global-auto-revert-mode t) ;; Automatically reload file and show changes if the file has changed
 
-  ;;(global-visual-line-mode t)           ;; Enable truncated lines
-  ;;(display-line-numbers-type 'relative) ;; Relative line numbers
   (global-display-line-numbers-mode t)  ;; Display line numbers
 	(global-hl-line-mode t) ;; destaca a linha que o cursor está
 
 
   (mouse-wheel-progressive-speed nil) ;; Disable progressive speed when scrolling
   (scroll-conservatively 10) ;; Smooth scrolling
-  ;;(scroll-margin 8)
 
   (tab-width 2)
 
@@ -39,12 +45,10 @@
   :hook
   (prog-mode . (lambda () (hs-minor-mode t))) ;; Enable folding hide/show globally
   :config
-  ;; Move customization variables to a separate file and load it, avoid filling up init.el with unnecessary variables
   (setq custom-file (locate-user-emacs-file "custom-vars.el"))
   (load custom-file 'noerror 'nomessage)
   :bind (
          ([escape] . keyboard-escape-quit) ;; Makes Escape quit prompts (Minibuffer Escape)
-         ;; Zooming In/Out
          ("C-+" . text-scale-increase)
          ("C--" . text-scale-decrease)
          ("<C-wheel-up>" . text-scale-increase)
@@ -61,14 +65,9 @@
 		eshell-mode-hook
 		shell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;; define	fonte
 (set-face-attribute 'default nil :font "Fira Code Retina" :height 140)
-
-;; performance
-;; The default is 800 kilobytes. Measured in bytes.
-(setq gc-cons-threshold (* 50 1000 1000))
-
-;; For blazingly fast startup times, this line makes startup miles faster
-(setq package-quickstart t)
 
 ;; make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -105,7 +104,7 @@
   :init
   (vertico-mode))
 
-(savehist-mode)
+(savehist-mode) ;; Enables save history mode
 
 (use-package marginalia
   :after vertico
@@ -118,6 +117,7 @@
   (nerd-icons-completion-mode)
   :hook
   ('marginalia-mode-hook . 'nerd-icons-completion-marginalia-setup))
+
 
 (use-package doom-themes
   :init(load-theme 'doom-gruvbox t))
@@ -132,6 +132,8 @@
   (evil-want-C-u-scroll t)      ;; Set C-u to scroll up
   (evil-want-C-i-jump nil)      ;; Disables C-i jump
   (evil-undo-system 'undo-redo) ;; C-r to redo
+
+
   ;; Unmap keys in 'evil-maps. If not done, org-return-follows-link will not work
   :bind (:map evil-motion-state-map
               ("SPC" . nil)
@@ -143,6 +145,7 @@
   ;; Setting where to use evil-collection
   (setq evil-collection-mode-list '(dired ibuffer magit corfu vertico consult info))
   (evil-collection-init))
+
 
 (use-package magit
   :defer
@@ -167,156 +170,20 @@
   :custom
   (org-roam-directory (file-truename "/home/edu/org-roam/"))
   :config
-  ;; If you're using a vertical completion framework, you might want a more informative completion interface
   (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
   (org-roam-db-autosync-mode)
-  ;; If using org-roam-protocol
   (require 'org-roam-protocol))
 
 (use-package perfect-margin
   :custom
   (perfect-margin-visible-width 128)
+
   :hook ((org-mode . perfect-margin-mode)
          (after-change-major-mode . perfect-margin--maybe-disable)))
 
-(defun perfect-margin--maybe-disable ()
-  (unless (derived-mode-p 'org-mode)
-    (perfect-margin-mode -1)))
-
-(use-package haskell-mode)
-
-(with-eval-after-load 'eglot
-	(add-to-list 'eglot-server-programs
-							 '(haskell-mode . ("haskell-language-server-wrapper" "--lsp"))))
-
-(setenv "PATH" (concat (getenv "PATH") ":" (expand-file-name "~/.ghcup/bin")))
-(add-to-list 'exec-path (expand-file-name "~/.ghcup/bin"))
-
-
-
-
-
-(use-package sideline-flymake
-  :hook (flymake-mode . sideline-mode)
-  :custom
-  (sideline-flymake-display-mode 'line) ;; Show errors on the current line
-  (sideline-backends-right '(sideline-flymake)))
-(use-package yasnippet-snippets
-  :hook (prog-mode . yas-minor-mode))(setq treesit-language-source-alist
-      '((bash "https://github.com/tree-sitter/tree-sitter-bash")
-				(haskell "https://github.com/tree-sitter/tree-sitter-haskell")
-        (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
-
-(defun start/install-treesit-grammars ()
-  "Install missing treesitter grammars"
-  (interactive)
-  (dolist (grammar treesit-language-source-alist)
-    (let ((lang (car grammar)))
-      (unless (treesit-language-available-p lang)
-        (treesit-install-language-grammar lang)))))
-
-;; Call this function to install missing grammars
-(start/install-treesit-grammars)
-
-;; Optionally, add any additional mode remappings not covered by defaults
-(setq major-mode-remap-alist
-      '((yaml-mode . yaml-ts-mode)
-        (sh-mode . bash-ts-mode)
-        (c-mode . c-ts-mode)
-        (c++-mode . c++-ts-mode)
-        (css-mode . css-ts-mode)
-        (python-mode . python-ts-mode)
-        (mhtml-mode . html-ts-mode)
-        (javascript-mode . js-ts-mode)
-        (json-mode . json-ts-mode)
-        (typescript-mode . typescript-ts-mode)
-        (conf-toml-mode . toml-ts-mode)
-        ))
-
-;; Or if there is no built in mode
-(use-package cmake-ts-mode :ensure nil :mode ("CMakeLists\\.txt\\'" "\\.cmake\\'"))
-(use-package go-ts-mode :ensure nil :mode "\\.go\\'")
-(use-package go-mod-ts-mode :ensure nil :mode "\\.mod\\'")
-(use-package rust-ts-mode :ensure nil :mode "\\.rs\\'")
-(use-package tsx-ts-mode :ensure nil :mode "\\.tsx\\'")
-
-(use-package corfu
-  ;; Optional customizations
-  :custom
-  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-  (corfu-auto t)                 ;; Enable auto completion
-  (corfu-auto-prefix 2)          ;; Minimum length of prefix for auto completion.
-  (corfu-popupinfo-mode t)       ;; Enable popup information
-  (corfu-popupinfo-delay 0.5)    ;; Lower popup info delay to 0.5 seconds from 2 seconds
-  (corfu-separator ?\s)          ;; Orderless field separator, Use M-SPC to enter separator
-  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
-  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
-  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
-  (completion-ignore-case t)
-
-  ;; Emacs 30 and newer: Disable Ispell completion function.
-  ;; Try `cape-dict' as an alternative.
-  (text-mode-ispell-word-completion nil)
-
-  ;; Enable indentation+completion using the TAB key.
-  ;; `completion-at-point' is often bound to M-TAB.
-  (tab-always-indent 'complete)
-
-  (corfu-preview-current nil) ;; Don't insert completion without confirmation
-  ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
-  ;; be used globally (M-/).  See also the customization variable
-  ;; `global-corfu-modes' to exclude certain modes.
-  :init
-  (global-corfu-mode))
-
-(use-package nerd-icons-corfu
-  :after corfu
-  :init (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
-
-(use-package cape
-  :after corfu
-  :init
-  (add-hook 'completion-at-point-functions #'cape-dabbrev) ;; Complete word from current buffers
-  (add-hook 'completion-at-point-functions #'cape-dict) ;; Dictionary completion
-  (add-hook 'completion-at-point-functions #'cape-file) ;; Path completion
-  (add-hook 'completion-at-point-functions #'cape-elisp-block) ;; Complete elisp in Org or Markdown mode
-  (add-hook 'completion-at-point-functions #'cape-keyword) ;; Keyword completion
-  )
-
-(use-package orderless
-  :custom
-  (completion-styles '(orderless basic))
-  (completion-category-overrides '((file (styles basic partial-completion)))))
-
-(use-package vertico
-  :init
-  (vertico-mode))
-
-(savehist-mode) ;; Enables save history mode
-
-(use-package marginalia
-  :after vertico
-  :init
-  (marginalia-mode))
-
-(use-package nerd-icons-completion
-  :after marginalia
-  :config
-  (nerd-icons-completion-mode)
-  :hook
-  ('marginalia-mode-hook . 'nerd-icons-completion-marginalia-setup))
-
-(use-package projectile
-  :config
-  (projectile-mode)
-  :custom
-  ;; (projectile-auto-discover nil) ;; Disable auto search for better startup times ;; Search with a keybind
-  (projectile-run-use-comint-mode t) ;; Interactive run dialog when running projects inside emacs (like giving input)
-  (projectile-switch-project-action #'projectile-dired) ;; Open dired when switching to a project
-  (projectile-project-search-path '("~/projects/" "~/work/" ("~/github" . 1)))) ;; . 1 means only search the first subdirectory level for projects
+	(defun perfect-margin--maybe-disable ()
+		(unless (derived-mode-p 'org-mode)
+			(perfect-margin-mode -1)))
 
 (use-package general
 	:config
@@ -337,22 +204,9 @@
 		"n c" '(org-roam-capture :wk "capture")
 		"n j" '(org-roam-dailies-capture-today :wk "capture today"))
 
-	(start/leader-keys
-    "e" '(:ignore t :wk "Languages")
-    "e e" '(eglot-reconnect :wk "Eglot Reconnect")
-    "e d" '(eldoc-doc-buffer :wk "Eldoc Buffer")
-    "e f" '(eglot-format :wk "Eglot Format")
-    "e l" '(consult-flymake :wk "Consult Flymake")
-    "e r" '(eglot-rename :wk "Eglot Rename")
-    "e i" '(xref-find-definitions :wk "Find definition")
-    "e v" '(:ignore t :wk "Elisp")
-    "e v b" '(eval-buffer :wk "Evaluate elisp in buffer")
-    "e v r" '(eval-region :wk "Evaluate elisp in region"))
-
   (start/leader-keys
     "g" '(:ignore t :wk "Git")
     "g s" '(magit-status :wk "Magit status"))
-
 
   (start/leader-keys
     "h" '(:ignore t :wk "Help") ;; To get more help use C-h commands (describe variable, function, etc.)
@@ -363,17 +217,14 @@
  (start/leader-keys
     "." '(find-file :wk "Find file")
     "TAB" '(comment-line :wk "Comment lines")
-    "q" '(flymake-show-buffer-diagnostics :wk "Flymake buffer diagnostic")
-    ;;"c" '(eat :wk "Eat terminal")
-    "p" '(projectile-command-map :wk "Projectile")
-    "s p" '(projectile-discover-projects-in-search-path :wk "Search for projects"))
+    "q" '(flymake-show-buffer-diagnostics :wk "Flymake buffer diagnostic"))
 
   (start/leader-keys
     "s" '(:ignore t :wk "Search")
     "s c" '((lambda () (interactive) (find-file "~/.config/emacs/init.org")) :wk "Find emacs Config")
     "s r" '(consult-recent-file :wk "Search recent files")
     "s f" '(consult-fd :wk "Search files with fd")
-    "s g" '(consult-ripgrep :wk "Search with ripgrep")
+    "s g" '(consult-ripgrep :wk "Search with ripgep")
     "s l" '(consult-line :wk "Search line")
     "s i" '(consult-imenu :wk "Search Imenu buffer locations")) ;; This one is really cool
 
@@ -388,36 +239,7 @@
     "d v" '(dired :wk "Open dired")
     "d j" '(dired-jump :wk "Dired jump to current")))
 
-(use-package consult
-  :hook (completion-list-mode . consult-preview-at-point-mode)
-  :init
-  ;; `consult-register-store' and the Emacs built-ins.
-  (setq register-preview-delay 0.5
-        register-preview-function #'consult-register-format)
-
-  (advice-add #'register-preview :override #'consult-register-window)
-
-  (setq xref-show-xrefs-function #'consult-xref
-        xref-show-definitions-function #'consult-xref)
-  :config
- (autoload 'projectile-project-root "projectile")
-  (setq consult-project-function (lambda (_) (projectile-project-root))))
 
 ;; remove espaço em branco do final das linhas
 (use-package ws-butler
   :init (ws-butler-global-mode))
-
-;; alternativa ao built-in do emacs
-(use-package helpful
-  :bind
-  ;; Note that the built-in `describe-function' includes both functions
-  ;; and macros. `helpful-function' is functions only, so we provide
-  ;; `helpful-callable' as a drop-in replacement.
-  ("C-h f" . helpful-callable)
-  ("C-h v" . helpful-variable)
-  ("C-h k" . helpful-key)
-  ("C-h x" . helpful-command))
-
-;; performance
-(setq gc-cons-threshold (* 2 1000 1000))
-(setq read-process-output-max (* 1024 1024)) ;; 1mb
